1adde803877dc214194ea8d38f99cb40f78231e2
Seyed Mostafa <aseyedmostafaho@gmail.com>
2020-01-07 09:26:15 -0500

    client connecting to server

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,28 +1,38 @@
 package main
 
 import (
-	"bytes"
-	"encoding/gob"
-	"io/ioutil"
-	"github.com/golang/protobuf/proto"
-	"todo/todo"
+	"log"
+	// "bytes"
+	// "encoding/gob"
+	// "io/ioutil"
+	// "github.com/golang/protobuf/proto"
+	context "context"
+	grpc "google.golang.org/grpc"
+
 	"flag"
 	"fmt"
 	"os"
 	"strings"
+	"todo/todo"
 )
 
-func main(){
+func main() {
 	flag.Parse()
 	if flag.NArg() < 1 {
 		fmt.Fprintln(os.Stderr, "missing subcommand: list or add")
 		os.Exit(1)
 	}
 
-	var err error
-	switch cmd := flag.Arg(0); cmd {
+	conn, err := grpc.Dial(":8888", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("could not connect to backend %v", err)
+	}
+	
+	client := todo.NewTasksClient(conn)
+
+ 	switch cmd := flag.Arg(0); cmd {
 	case "list":
-		err = list()
+		err = list(context.Background(), client)
 	case "add":
 		err = add(strings.Join(flag.Args()[1:], " "))
 	default:
@@ -35,77 +45,26 @@ func main(){
 	// fmt.Println("TODO")
 }
 
-const dbPath = "mydb.pb"
+var dbPath = "mydb.pb"
 
-func  add(text string) error {
-	task := &todo.Task {
-		Text: text,
-		Done: false,
-	}
-
-
-	b, err := proto.Marshal(task)
-	if err != nil {		
-		return fmt.Errorf("Could not encode task: %v", err)
-	}
-	
-	f, err := os.OpenFile(dbPath, os.O_WRONLY | os.O_CREATE | os.O_APPEND, 0666 )
-	
-	if err != nil {		
-		return fmt.Errorf("Could not open %s: %v", dbPath, err)
-	}
-
-	if err := gob.NewEncoder(f).Encode(int64(len(b))); err != nil {
-		return fmt.Errorf("Could not encode length of message %v", err)
-	}
-	_, err = f.Write(b)
-	if err != nil {		
-		return fmt.Errorf("Could not write task to file: %v", err)
-	}
-
-	if err := f.Close(); err != nil {
-		return fmt.Errorf("Could not close file %s: %v", dbPath, err)		
-	}
-	
-	// fmt.Println(task.GetDone())
-	// fmt.Println(task.GetText())
-
-	fmt.Println(proto.MarshalTextString(task))
-	return nil
+func add(text string) error {
+	return fmt.Errorf("add not implemented")
 }
 
-func list() error {
-	b, err := ioutil.ReadFile(dbPath)
-	if err != nil {		
-		return fmt.Errorf("Could not read file %s: %v",dbPath, err)
+func list(ctx context.Context, client todo.TasksClient) error {
+	l, err := client.List(ctx, &todo.Void{})
+	if err != nil {
+		return fmt.Errorf("could not fetch tasks %v", err)
 	}
 
-	for {
-		if len(b) == 0 {
-			return nil
-		}else if len(b) < 4 {
-			return fmt.Errorf("remaining odd %d bytes, what to do?", len(b))
-		}
-
-		var length int64
-		if err := gob.NewDecoder(bytes.NewReader(b[:4])).Decode(&length); err != nil {
-			return fmt.Errorf("Could not decode message length: %v", err)
-		}
-		b = b[4:]
-		
-		var task todo.Task
-		if err := proto.Unmarshal(b[:length], &task); err != nil {
-			return fmt.Errorf("Could not read task: %v", err)
-		}
-		b = b[length:]
-
-		if task.Done {
-			fmt.Printf ("[X]")
-		}else{
+	for _, t := range l.Tasks {
+		if t.Done {
+			fmt.Printf("[X]")
+		} else {
 			fmt.Printf("[ ]")
 		}
 
-		fmt.Printf(" %s\n",task.Text)
+		fmt.Printf(" %s\n", t.Text)
 	}
-	
+	return nil
 }
