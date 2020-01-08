6142737cbaf51977626d89ef215c5db2a0beb571
Seyed Mostafa <aseyedmostafaho@gmail.com>
2020-01-08 02:18:00 -0500

    config a client to work with server

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"log"
+	// "log"
 	// "bytes"
 	// "encoding/gob"
 	// "io/ioutil"
@@ -25,18 +25,18 @@ func main() {
 
 	conn, err := grpc.Dial(":8888", grpc.WithInsecure())
 	if err != nil {
-		log.Fatalf("could not connect to backend %v", err)
+		fmt.Fprintf(os.Stderr, "could not connect to backend: %v\n", err)
+		os.Exit(1)
 	}
-	
 	client := todo.NewTasksClient(conn)
 
- 	switch cmd := flag.Arg(0); cmd {
+	switch cmd := flag.Arg(0); cmd {
 	case "list":
 		err = list(context.Background(), client)
 	case "add":
-		err = add(strings.Join(flag.Args()[1:], " "))
+		err = add(context.Background(), client, strings.Join(flag.Args()[1:], " "))
 	default:
-		err = fmt.Errorf("Unknown subommand %s", cmd)
+		err = fmt.Errorf("unknown subcommand %s", cmd)
 	}
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
@@ -47,23 +47,27 @@ func main() {
 
 var dbPath = "mydb.pb"
 
-func add(text string) error {
-	return fmt.Errorf("add not implemented")
+func add(ctx context.Context, client todo.TasksClient, text string) error {
+	_, err := client.Add(ctx, &todo.Text{Text: text})
+	if err != nil {
+		return fmt.Errorf("could not add task in the backend: %v", err)
+	}
+
+	fmt.Println("task added successfully")
+	return nil
 }
 
 func list(ctx context.Context, client todo.TasksClient) error {
 	l, err := client.List(ctx, &todo.Void{})
 	if err != nil {
-		return fmt.Errorf("could not fetch tasks %v", err)
+		return fmt.Errorf("could not fetch tasks: %v", err)
 	}
-
 	for _, t := range l.Tasks {
 		if t.Done {
 			fmt.Printf("[X]")
 		} else {
 			fmt.Printf("[ ]")
 		}
-
 		fmt.Printf(" %s\n", t.Text)
 	}
 	return nil
